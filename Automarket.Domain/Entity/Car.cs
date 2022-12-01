1ceb2a21058a8989ca1019ea514545fae6f7d87d
Grindelwalt <Sulxj600s@gmail.com>
2022-12-01 13:17:24 +0300

    исправленны ошибки в представлении car добавлено свойство Description в Car

--- Automarket.Domain/Entity/Car.cs
+++ Automarket.Domain/Entity/Car.cs
@@ -10,12 +10,13 @@ namespace Automarket.Domain.Entity
         public string Brend { get; set; }
         public string Model { get; set; }
         public string Generation { get; set; }
-        public DateTime DareCreate { get; set; }
+        public DateTime DateCreate { get; set; }
         public string Power { get; set; }
         public int Price { get; set; }
         public string Calour { get; set; }
         public string Img { get; set; }
         public TypeCar TypeCar { get; set; }
         public List<int> LinksImages { get; set; }
+        public string Description { get; set; }
     }
 }
