1d8fc3e783693d814651ace4d6f22fe21fd90814
Grindelwalt <Sulxj600s@gmail.com>
2022-12-05 20:58:04 +0300

    дабавленны роли

---
+++ Automarket.Domain/Enam/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Automarket.Domain.Enam
+{
+    public enum Role
+    {
+        [Display(Name = "Пользователь")]
+        User = 0,
+        [Display(Name = "Модератор")]
+        Moderator = 1,
+        [Display(Name = "Админ")]
+        Admin = 2,
+    }
+}
