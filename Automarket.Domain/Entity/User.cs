62b4fb67688f18562ebdb78a4d227b324b40e1fc
Grindelwalt <Sulxj600s@gmail.com>
2022-12-05 20:49:34 +0300

    Создана модель пользователя

---
+++ Automarket.Domain/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿using Automarket.Domain.Enam;
+
+namespace Automarket.Domain.Entity
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
