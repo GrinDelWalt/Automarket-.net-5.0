6d557a3db3a7da3ba322aa622f24672326bfbee6
Grindelwalt <Sulxj600s@gmail.com>
2022-12-05 21:00:41 +0300

    сервисы логирования

---
+++ Automarket.Service/Interfeces/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using Automarket.Domain.Response;
+using Automarket.Domain.ViewModels.Account;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Automarket.Service.Interfeces
+{
+    public interface IAccountService
+    {
+        Task<BaseResponse<ClaimsIdentity>> Register(RegisterViewModel model);
+
+        Task<BaseResponse<ClaimsIdentity>> Login(LoginViewModel model);
+
+        Task<BaseResponse<bool>> ChangePassword(ChangePasswordViewModel model);
+    }
+}
