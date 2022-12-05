1c241c947d79a1d0af32a99623dcf526e50ae642
Grindelwalt <Sulxj600s@gmail.com>
2022-12-06 01:00:25 +0300

    view Login

--- Automarket.DAL/Repository/ProfileRepository.cs
+++ Automarket.DAL/Repository/ProfileRepository.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Automarket.DAL.Interfeces;
+using Automarket.Domain.Entity;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Automarket.DAL.Repository
 {
-    internal class ProfileRepository
+    public class ProfileRepository : IBaseRepository<Profile>
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProfileRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Create(Profile entity)
+        {
+            await _dbContext.Profiles.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public IQueryable<Profile> GetAll()
+        {
+            return _dbContext.Profiles;
+        }
+
+        public async Task<bool> Delete(Profile entity)
+        {
+            _dbContext.Profiles.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Profile> Update(Profile entity)
+        {
+            _dbContext.Profiles.Update(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
     }
 }
