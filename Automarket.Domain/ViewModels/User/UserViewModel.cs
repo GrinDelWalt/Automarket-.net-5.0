a98d2272f838a3a64b306a26ae4357a7012520be
Grindelwalt <Sulxj600s@gmail.com>
2022-12-06 02:04:34 +0300

    добавлен интрерфейс и сервис Profile исправление ошибок

--- Automarket.Domain/ViewModels/User/UserViewModel.cs
+++ Automarket.Domain/ViewModels/User/UserViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Automarket.Domain.ViewModels.User
 {
-    internal class UserViewModel
+    public class UserViewModel
     {
+        [Display(Name = "Id")]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Укажите роль")]
+        [Display(Name = "Роль")]
+        public string Role { get; set; }
+
+        [Required(ErrorMessage = "Укажите логин")]
+        [Display(Name = "Логин")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите пароль")]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
     }
 }
