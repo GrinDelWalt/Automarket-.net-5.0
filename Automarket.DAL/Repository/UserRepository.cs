146fb748aaf0cfca99abf0ca5f7296cfbf48a93e
Grindelwalt <Sulxj600s@gmail.com>
2022-12-05 21:00:21 +0300

    Модель профиля

--- Automarket.DAL/Repository/UserRepository.cs
+++ Automarket.DAL/Repository/UserRepository.cs
@@ -1,9 +1,6 @@
 ﻿using Automarket.DAL.Interfeces;
 using Automarket.Domain.Entity;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Automarket.DAL.Repository
