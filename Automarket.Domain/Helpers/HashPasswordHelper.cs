8b33a44eab5609ad6ba6fbf026b57fe5032ae6b1
Grindelwalt <Sulxj600s@gmail.com>
2022-12-05 21:01:00 +0300

    кодировка поролей

---
+++ Automarket.Domain/Helpers/HashPasswordHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Automarket.Domain.Helpers
+{
+    public static class HashPasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                return hash;
+            }
+        }
+    }
+}
